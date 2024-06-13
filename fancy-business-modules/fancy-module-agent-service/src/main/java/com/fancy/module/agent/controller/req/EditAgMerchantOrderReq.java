193de5a8aa00fd66ab509d3f76839d2b8c2f1c7d
shitou <liukaixun@fancy.tech>
2024-06-13 16:12:54 +0800

    创建订单

--- fancy-business-modules/fancy-module-agent-service/src/main/java/com/fancy/module/agent/controller/req/EditAgMerchantOrderReq.java
+++ fancy-business-modules/fancy-module-agent-service/src/main/java/com/fancy/module/agent/controller/req/EditAgMerchantOrderReq.java
@@ -1,20 +1,83 @@
 package com.fancy.module.agent.controller.req;
 
-import com.fancy.module.agent.repository.pojo.AgMerchantOrder;
-import com.fancy.module.agent.repository.pojo.AgMerchantOrderDetail;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
 
+import java.math.BigDecimal;
 import java.util.List;
 
 @Data
 @Accessors(chain = true)
 @EqualsAndHashCode(callSuper = false)
-public class EditAgMerchantOrderReq extends AgMerchantOrder {
+public class EditAgMerchantOrderReq  {
+    /**
+     * 代理商户id
+     */
+    @NotNull(message = "代理商户id不能为空")
+    private Long agMerchantId;
+
+    /**
+     * 代理商户名称
+     */
+    @NotBlank(message = "代理商户名称不能为空")
+    private String name;
+
+    /**
+     * fc商户id
+     */
+    @NotNull(message = "fc商户id不能为空")
+    private Long merchantId;
+
 
     /**
      * 订单明细
      */
-    private List<AgMerchantOrderDetail> orderDetailList;
+    @NotEmpty(message = "订单明细不能为空")
+    private List<@Valid OrderDetail> orderDetailList;
+
+    /**
+     * 订单金额
+     */
+    private BigDecimal orderMoney;
+
+    @Data
+    @Accessors(chain = true)
+    @EqualsAndHashCode(callSuper = false)
+    public static class OrderDetail{
+        /**
+         * 服务版本类型
+         */
+        @NotNull(message = "服务版本Id不能为空")
+        private Long contentServiceId;
+        /**
+         * 服务类型
+         */
+        @NotNull(message = "服务类型不能为空")
+        private Integer serviceType;
+        /**
+         * 覆盖数
+         */
+        @NotNull(message = "覆盖数不能为空")
+        private Integer coverageNumber;
+        /**
+         * 生成数
+         */
+        @NotNull(message = "生成数不能为空")
+        private Integer numberOfGenerations;
+
+        /**
+         * 订单金额
+         */
+        private BigDecimal orderSubMoney;
+
+        /**
+         * 服务任务数
+         */
+        private Integer serviceTotalNum;
+    }
 }
