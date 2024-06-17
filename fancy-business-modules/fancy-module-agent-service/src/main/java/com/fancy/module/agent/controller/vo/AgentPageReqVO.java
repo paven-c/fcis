90589e9322e6868e921f12a11bef3097874fcdd7
yanyi <paven_c@163.com>
2024-06-17 14:32:41 +0800

    日期格式修改

--- fancy-business-modules/fancy-module-agent-service/src/main/java/com/fancy/module/agent/controller/vo/AgentPageReqVO.java
+++ fancy-business-modules/fancy-module-agent-service/src/main/java/com/fancy/module/agent/controller/vo/AgentPageReqVO.java
@@ -1,13 +1,16 @@
 package com.fancy.module.agent.controller.vo;
 
 import com.fancy.common.pojo.PageParam;
+import com.fancy.common.util.date.DateUtils;
 import com.fancy.common.validation.InEnum;
 import com.fancy.module.agent.enums.AgentStatusEnum;
 import io.swagger.v3.oas.annotations.media.Schema;
+import java.time.LocalDateTime;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 /**
  * @author paven
@@ -34,6 +37,14 @@ public class AgentPageReqVO extends PageParam {
     @Schema(description = "代理等级", example = "")
     @InEnum(value = AgentStatusEnum.class, message = "代理商类型必须是 {status}")
     private Integer level;
+
+    @Schema(description = "创建时间开始", example = "")
+    @DateTimeFormat(pattern = DateUtils.FORMAT_YEAR_MONTH_DAY_HOUR_MINUTE_SECOND)
+    private LocalDateTime startTime;
+
+    @Schema(description = "创建时间结束", example = "")
+    @DateTimeFormat(pattern = DateUtils.FORMAT_YEAR_MONTH_DAY_HOUR_MINUTE_SECOND)
+    private LocalDateTime endTime;
 
     private Boolean isCompanyRole;
 
--- fancy-common-components/fancy-spring-boot-starter-web/src/main/java/com/fancy/component/jackson/config/JacksonAutoConfiguration.java
+++ fancy-common-components/fancy-spring-boot-starter-web/src/main/java/com/fancy/component/jackson/config/JacksonAutoConfiguration.java
@@ -1,10 +1,15 @@
 package com.fancy.component.jackson.config;
 
 import cn.hutool.core.collection.CollUtil;
+import com.fancy.common.util.date.DateUtils;
 import com.fancy.common.util.json.JsonUtils;
 import com.fancy.component.jackson.core.databind.NumberSerializer;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
@@ -25,7 +30,11 @@ public class JacksonAutoConfiguration {
         simpleModule
                 // 新增 Long 类型序列化规则，数值超过 2^53-1，在 JS 会出现精度丢失问题，因此 Long 自动序列化为字符串类型
                 .addSerializer(Long.class, NumberSerializer.INSTANCE)
-                .addSerializer(Long.TYPE, NumberSerializer.INSTANCE);
+                .addSerializer(Long.TYPE, NumberSerializer.INSTANCE)
+                .addSerializer(
+                        LocalDateTime.class, new LocalDateTimeSerializer(DateTimeFormatter.ofPattern(DateUtils.FORMAT_YEAR_MONTH_DAY_HOUR_MINUTE_SECOND)))
+                .addDeserializer(
+                        LocalDateTime.class, new LocalDateTimeDeserializer(DateTimeFormatter.ofPattern(DateUtils.FORMAT_YEAR_MONTH_DAY_HOUR_MINUTE_SECOND)));
 //                .addSerializer(LocalDate.class, LocalDateSerializer.INSTANCE)
 //                .addDeserializer(LocalDate.class, LocalDateDeserializer.INSTANCE)
 //                .addSerializer(LocalTime.class, LocalTimeSerializer.INSTANCE)
